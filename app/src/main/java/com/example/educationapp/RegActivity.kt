1ade986ec7a50f7f58350ede6258304c4b4cf8d7
Milolika1221 <153797512+Milolika1221@users.noreply.github.com>
2025-05-31 18:35:18 +0700

    Update RegActivity.kt

--- app/src/main/java/com/example/educationapp/RegActivity.kt
+++ app/src/main/java/com/example/educationapp/RegActivity.kt
@@ -8,6 +8,8 @@ import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
     // Firebase

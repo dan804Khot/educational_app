c4e291ecb18c45e78b27aaf5cbd6fb7c31077fff
Milolika1221 <153797512+Milolika1221@users.noreply.github.com>
2025-05-31 18:34:59 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/educationapp/MainActivity.kt
+++ app/src/main/java/com/example/educationapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     // Инициализация Firebase Auth
